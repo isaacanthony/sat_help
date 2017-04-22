ffbd2ef4ebaf6d5c046f466292f9dfea52d19174
isaac <isaac@isaac-AOD255.(none)>
2017-04-22 18:20:13 -0400

    Pull random row from questions.

--- api.js
+++ api.js
@@ -1,9 +1,30 @@
+QID = 'SECRET';
+
 function doGet(e) {
-  var params = JSON.stringify(e);
-  return HtmlService.createHtmlOutput(params);
+  if (!e || !e.parameter) {
+    return HtmlService.createHtmlOutput('Invalid parameters.');
+  } else {
+    // var subject = e.parameter.subject;
+    var sheet = SpreadsheetApp.openById(QID).getSheets()[0];
+    var max = sheet.getLastRow();
+    var rowId = Math.floor(Math.random() * max) + 1;    
+    var row = sheet.getRange('A' + rowId + ':G' + rowId).getValues()[0];
+    
+    var data = {
+      'id': row[0],
+      'q': row[1],
+      'a': row[2],
+      'b': row[3],
+      'c': row[4],
+      'd': row[5],
+      'ans': row[6]
+    };
+    
+    return HtmlService.createHtmlOutput(JSON.stringify(data));
+  }
 };
 
-function doPost(e) { 
+function doPost(e) {
   var params = JSON.stringify(e);
   return HtmlService.createHtmlOutput(params);
 };
