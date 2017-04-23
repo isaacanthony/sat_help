327e1bd8bf394214717b66d97ab6186e469245ee
isaac <isaac@isaac-AOD255.(none)>
2017-04-23 18:38:11 -0400

    Post answer to back-end.

--- api.js
+++ api.js
@@ -1,30 +1,44 @@
-QID = 'SECRET';
+var QID = 'SECRET';
 
-function doGet(e) {
-  if (!e || !e.parameter || !e.parameter.subject || !e.parameter.callback) {
-    return HtmlService.createHtmlOutput('Invalid parameters.');
-  } else {
-    var sheet = SpreadsheetApp.openById(QID).getSheets()[0];
-    var max = sheet.getLastRow();
-    var rowId = Math.floor(Math.random() * max) + 1;    
-    var row = sheet.getRange('A' + rowId + ':G' + rowId).getValues()[0];
-    
-    var data = {
-      'id': row[0],
-      'q': row[1],
-      'a': row[2],
-      'b': row[3],
-      'c': row[4],
-      'd': row[5],
-      'ans': row[6]
-    };
-    
-    return ContentService.createTextOutput(e.parameter.callback + '(' + JSON.stringify(data) + ')').setMimeType(ContentService.MimeType.JAVASCRIPT);
-  }
+function invalidParameters() {
+  return { 'error': 'Invalid parameters' };
 };
 
-function doPost(e) {
-  var params = JSON.stringify(e);
-  return HtmlService.createHtmlOutput(params);
+function getQuestion(e) {
+  // var subject = e.parameter.subject;
+  var sheet = SpreadsheetApp.openById(QID).getSheets()[0];
+  var max = sheet.getLastRow();
+  var rowId = Math.floor(Math.random() * max) + 1;    
+  var row = sheet.getRange('A' + rowId + ':G' + rowId).getValues()[0];
+  
+  return {
+    'id': row[0],
+    'q': row[1],
+    'a': row[2],
+    'b': row[3],
+    'c': row[4],
+    'd': row[5],
+    'ans': row[6]
+  };
+};
+
+function postAnswer(e) {
+  return { 'success': true };
+};
+
+function doGet(e) {
+  var data = {};
+  
+  if (!e.parameter.callback) {
+    data = invalidParameters();
+  } else if (e.parameter.subject) {
+    data = getQuestion(e);
+  } else if (e.parameter.answer) {
+    data = postAnswer(e);
+  } else {
+    data = invalidParameters();
+  }
+  
+  return ContentService.createTextOutput(e.parameter.callback + '(' + JSON.stringify(data) + ')').setMimeType(ContentService.MimeType.JAVASCRIPT);
 };
 
